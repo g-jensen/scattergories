3a8e6704478d96b3cbeb62bfa57c788e604f9242
g-jensen <greg.micah.jensen@gmail.com>
2024-02-29 05:42:54 -0500

    nickname->nickname-ratom

--- src/cljs/scattergories/home.cljs
+++ src/cljs/scattergories/home.cljs
@@ -12,18 +12,18 @@
   (when nickname
     (ws/call! :room/create {:nickname nickname} join-room!)))
 
-(defn home [nickname]
+(defn home [nickname-ratom]
   [:div.homepage-container
    [:h1 "Welcome to Scattergories"]
    [:div.nickname-input
     [:input {:type "text"
              :id "-nickname-input"
              :placeholder "Enter your nickname"
-             :value @nickname
-             :on-change #(reset! nickname (wjs/e-text %))}]]
+             :value @nickname-ratom
+             :on-change #(reset! nickname-ratom (wjs/e-text %))}]]
    [:div.room-actions
     [:button {:id       "-create-room-button"
-              :on-click #(create-room! @nickname)}
+              :on-click #(create-room! @nickname-ratom)}
      "Create Room"]]])
 
 (defmethod page/render :home [_]
